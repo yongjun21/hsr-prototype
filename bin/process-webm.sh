0a6905533f800f61790581b2977fab5605d69f16
Yong Jun <yjthong.2009@smu.edu.sg>
2019-01-29 13:48:52 +0800

    Resize webm for smaller filesize

--- bin/process-webm.sh
+++ bin/process-webm.sh
@@ -5,9 +5,9 @@ cd video
 function process {
   if [[ -z $MOBILE ]]
   then
-    ffmpeg -i raw/$1.webm -c:v libvpx-vp9 -b:v 0 -crf 42 -g 30 -an -threads 7 -vf crop=1920:1200:0:120,eq=saturation=1.3:brightness=-0.01,unsharp=5:5:0.5:5:5:0.0 -speed 0 processed/$1.webm
+    ffmpeg -i raw/$1.webm -c:v libvpx-vp9 -b:v 0 -crf 42 -g 30 -an -threads 7 -vf crop=1920:1200:0:120,eq=saturation=1.3:brightness=-0.01,scale=1600:1000,unsharp=5:5:1:5:5:0.0 -speed 0 processed/$1.webm
   else
-    ffmpeg -i raw/mobile/$1.webm -c:v libvpx-vp9 -b:v 0 -crf 42 -g 30 -an -threads 7 -vf crop=414:736:0:120,eq=saturation=1.3:brightness=-0.01,unsharp=5:5:0.5:5:5:0.0 -speed 0 processed/mobile/$1.webm
+    ffmpeg -i raw/mobile/$1.webm -c:v libvpx-vp9 -b:v 0 -crf 42 -g 30 -an -threads 7 -vf crop=414:736:0:120,eq=saturation=1.3:brightness=-0.01,unsharp=5:5:0.5:5:5:0.0 processed/mobile/$1.webm
   fi
 }
 
